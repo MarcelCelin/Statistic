ccd26a0226bcc10cb6fc8506d574bffe8eff39bc
MarcelCelin <yonguecelin@gmail.com>
2024-12-08 22:32:39 +0100

    user list

--- Frontend/src/app/app.route.ts
+++ Frontend/src/app/app.route.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { AppComponent } from './app.component';
+import { UserListComponentComponent } from './user-list-component/user-list-component.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
   { path: 'home', component: MenuComponent },
   { path: 'add-member', component: AddUserComponent },
+  {path: 'member-list', component: UserListComponentComponent},
   { path: '**', redirectTo: '/home' },                     
 ];
